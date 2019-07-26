093682221c2443f2b8859445280a9ac6bd161d98
newstreetpunk <newstreetpunk@yandex.ru>
2019-07-26 18:54:29 +0500

    4355

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
 $(function() {
 
-	// Custom JS
+	var main = function() {
+		$('.menu-btn').click(function() {
+			$('.info-block').css('left', '0px');
+		});
+	};
 
+	$(document).ready(main);
+
+	var rahul=0;
+	$(window).click(function(e) {
+		$('.info-block').css('left', '-370px');   
+	});
+
+	
 });
