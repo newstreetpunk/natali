961ae768bc7d5663208ae8232d460e7605aaf89b
newstreetpunk <newstreetpunk@yandex.ru>
2019-07-27 20:42:07 +0500

    Page

--- js/common.js
+++ js/common.js
@@ -18,5 +18,17 @@ $(function() {
 		$('.info-block').css('left', '-370px');   
 	});
 
+	var winH = $(window).height();
+		$('.info-block').css('height', winH + 'px');
+
+	var html = $('.title-page, .double-title-page').html();
+    // doing the transformation (adding the span) with a regular expression
+    html = html.replace(/^([^\s]*)(.*)/, "<span class=\"first-word\">$1</span>$2");
+    // update your text
+    $('.title-page, .double-title-page').html(html);
+
+    // or in one line:
+    //$('.title-page, .double-title-page').html( ('.title-page, .double-title-page').html().replace(/^([^\s]*)(.*)/, "<span class=\"first-word\">$1</span>$2") );
+
 	
 });
