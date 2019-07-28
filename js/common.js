a11243f6f68d52bbf6fc58cd78b2e9e099c2bed7
newstreetpunk <newstreetpunk@yandex.ru>
2019-07-28 12:59:30 +0500

    123

--- js/common.js
+++ js/common.js
@@ -21,5 +21,11 @@ $(function() {
 	var winH = $(window).height();
 		$('.info-block').css('height', winH + 'px');
 
+	$('li:not(.main-menu ul li)').html(function(i, html){
+		var li = $(this).text();
+		var b = li.substring(0,2);
+		return html.replace(b, '<span class="wh">$&</span>');
+	});
+
 	
 });
